f25c9bcd6847771024e72f4b14b02c892dffe342
dodotronix <dodotronix@gmail.com>
2022-01-08 22:28:43 +0100

    i3 config

---
+++ i3/devel/screens.sh
@@ -0,0 +1,5 @@
+#/usr/bin/sh
+# display placement
+# this script is used in /etc/lightdm/lightdm.conf
+#xrandr --output VGA1 --auto --left-of LVDS1 
+#xrandr --output DP1 --mode 3840x2160 --left-of LVDS1 
---
+++ i3/todo.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+# find workspace "todo" in actual tree and check if there is already any window
+# only possible window is todo list in vim editor
+OUTPUT="$(i3-msg -t get_tree | jq '.nodes | .[] | .nodes | .[] | .nodes 
+                                   | .[] | select(.type=="workspace") 
+                                   | select(.name=="5:todo") | .nodes')"
+
+if [ -z "$OUTPUT" ]; then
+  alacritty --title "todo-list" -e nvim $HOME/Dropbox/TIN/index.wiki
+fi
