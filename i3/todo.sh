ec72b335ce3acb0469d58e0b97b20e3b85ddcf59
dodotronix <dodotronix@gmail.com>
2022-09-03 22:34:39 +0200

    task wiki reopen if close when switching to the workspace

--- i3/todo.sh
+++ i3/todo.sh
@@ -2,10 +2,10 @@
 
 # find workspace "todo" in actual tree and check if there is already any window
 # only possible window is todo list in vim editor
-OUTPUT="$(i3-msg -t get_tree | jq '.nodes | .[] | .nodes | .[] | .nodes 
-                                   | .[] | select(.type=="workspace") 
-                                   | select(.name=="5:todo") | .nodes')"
+OUTPUT=$(i3-msg -t get_tree | jq '.nodes | .[] | .nodes | .[] 
+                              | .nodes | .[] | .nodes | .[] | 
+                                select(.name == "todo-list") | .name')
 
 if [ -z "$OUTPUT" ]; then
-  alacritty --title "todo-list" -e nvim $HOME/projects/vimwiki_record/index.wiki
+  alacritty --title "todo-list" -e nvim $HOME/projects/vimwiki_record/index.wiki &
 fi
