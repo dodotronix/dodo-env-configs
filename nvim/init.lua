87f0312fd70ad24fce580f68ac4cf8a652ce04a7
dodotronix <dodotronix@gmail.com>
2022-10-09 23:37:26 +0200

    titles for alacritty window

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,6 +6,7 @@ vim.o.runtimepath = vim.o.runtimepath..',~/.local/share/nvim/site/pack/packer/st
 vim.g.netrw_local_delete_recursive=1
 
 vim.opt.number = true
+vim.opt.title = true
 vim.opt.relativenumber = true
 vim.opt.smartindent = true
 vim.opt.wrap = false
