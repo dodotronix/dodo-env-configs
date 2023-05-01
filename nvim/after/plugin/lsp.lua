daa7045d20da2ccbf8dd6bcb69390a08e75776d5
dodotronix <dodotronix@gmail.com>
2023-05-01 23:18:26 +0200

    added clangd and reorganized language server settings

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -5,57 +5,55 @@ local lsp_flags = {
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
---[[ require'lspconfig'.verible.setup{
-	on_attach = function(client, bufnr)
-        local bufopts = { noremap=true, silent=true, buffer=bufnr }
-        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-		vim.keymap.set("n", "gt", vim.lsp.buf.implementation, bufopts)
-		vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, bufopts)
-		vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, bufopts)
-	end,
-    root_dir = function() return vim.loop.cwd() end
-} ]]
---[[ require('lspconfig')['svls'].setup{
-    cmd = { "svls", "-d" },
-	capabilities = capabilities,
-	on_attach = function()
+local config_on_attach = function ()
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
-		vim.keymap.set("n", "gt", vim.lsp.buf.implementation, {buffer=0})
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
+		vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
+		vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer=0})
 		vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, {buffer=0})
 		vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, {buffer=0})
-	end,
-	flags = lsp_flags,
-} ]]
+        vim.keymap.set('n', '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, {})
+end
+
+require('lspconfig')["clangd"].setup{
+    capabilities = capabilities,
+    on_attach = config_on_attach,
+}
+
+
+require('lspconfig')["verible"].setup{
+	capabilities = capabilities,
+	on_attach = config_on_attach,
+    root_dir = function()
+        local a = vim.loop.cwd()
+        return a
+    end,
+    -- root_dir = function() return vim.loop.cwd() end
+}
+
 require('lspconfig')['pyright'].setup{
 	capabilities = capabilities,
-	on_attach = function()
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
-		vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
-		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
-		vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer=0})
-		vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, {buffer=0})
-		vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, {buffer=0})
-	end,
+	on_attach = config_on_attach,
 	flags = lsp_flags,
 }
+
 require('lspconfig')['lua_ls'].setup{
 	capabilities = capabilities,
-	on_attach = function()
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
-		vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
-		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
-		vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer=0})
-		vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, {buffer=0})
-		vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, {buffer=0})
-	end,
+	on_attach = config_on_attach,
 	flags = lsp_flags,
 	settings = {
 		Lua = {
-			diagnostics = { globals = {  'vim' } }
+			diagnostics = { globals = {  'vim' }, },
+            workspace = {
+                library = {
+                    vim.api.nvim_get_runtime_file("", true),
+                    [vim.fn.expand("$VIMRUNTIME/lua/vim")] = true,
+                    [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+                },
+            }
 		},
 	}
 }
