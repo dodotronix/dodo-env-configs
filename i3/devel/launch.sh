ef3f66d9b8acc3fa61f57b20169c5d114cc5680c
dodotronix <dodotronix@gmail.com>
2023-05-30 22:27:50 +0200

    deleted redundant devel configs from i3 config

--- i3/devel/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sh
-
-actual_path=$(pwd)
-
-cd $HOME/.config/i3
-head -n 10 common_config > config
-cat platform_spec_config >> config
-tail -n +10 common_config >> config 
-cd $actual_path
