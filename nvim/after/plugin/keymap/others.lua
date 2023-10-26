548996cb1e300753bd20950572a1f19de36cde5f
dodotronix <dodotronix@gmail.com>
2023-10-26 23:10:28 +0200

    added keymappings to center view when i go half page down and up

--- nvim/after/plugin/keymap/others.lua
+++ nvim/after/plugin/keymap/others.lua
@@ -2,3 +2,8 @@
 local opts = { noremap = true, silent = true }
 
 vim.keymap.set('n', '<leader>m', ':w<CR>:source %<CR>', opts)
+
+-- Normal-mode commands
+vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
+vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
+vim.keymap.set('x', '<leader>p', '"_dP', opts)
