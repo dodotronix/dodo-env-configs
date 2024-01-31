c767d38d086272c9b4655576085220c6577d3aae
Petr Pacner <petr.pacner@cern.ch>
2024-01-31 13:58:43 +0100

    updated lsp config using mason

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,60 +1,91 @@
-local lsp_flags = {
-  -- This is the default in Nvim 0.7+
-  debounce_text_changes = 150,
-}
+--  This function gets run when an LSP connects to a particular buffer.
+local on_attach = function(_, bufnr)
+    local nmap = function(keys, func, desc)
+        if desc then
+            desc = 'LSP: ' .. desc
+        end
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+    end
 
-local config_on_attach = function ()
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
-		vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
-		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
-		vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer=0})
-		vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next, {buffer=0})
-		vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, {buffer=0})
-        vim.keymap.set('n', '<space>wl', function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, {})
+    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+    nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+    nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+    nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+    nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+    nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+    -- See `:help K` for why this keymap
+    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+    nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+    -- Lesser used LSP functionality
+    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+    nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+    nmap('<leader>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, '[W]orkspace [L]ist Folders')
+
+    -- Create a command `:Format` local to the LSP buffer
+    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+        vim.lsp.buf.format()
+    end, { desc = 'Format current buffer with LSP' })
 end
 
-require('lspconfig')["clangd"].setup{
-    capabilities = capabilities,
-    on_attach = config_on_attach,
-}
+-- mason-lspconfig requires that these setup functions are called in this order
+-- before setting up the servers.
+require('mason').setup()
+require('mason-lspconfig').setup()
 
+local servers = {
+    -- clangd = {},
+    verible = {
+        root_dir = function()
+            local a = vim.loop.cwd()
+            return a
+        end },
+    lua_ls = {
+        Lua = {
+            workspace = { checkThirdParty = false },
+            telemetry = { enable = false },
 
-require('lspconfig')["verible"].setup{
-	capabilities = capabilities,
-	on_attach = config_on_attach,
-    root_dir = function()
-        local a = vim.loop.cwd()
-        return a
-    end,
-    -- root_dir = function() return vim.loop.cwd() end
-}
-
-require('lspconfig')['pyright'].setup{
-	capabilities = capabilities,
-	on_attach = config_on_attach,
-	flags = lsp_flags,
-}
-
-require('lspconfig')['lua_ls'].setup{
-	capabilities = capabilities,
-	on_attach = config_on_attach,
-	flags = lsp_flags,
-	settings = {
-		Lua = {
-			diagnostics = { globals = {  'vim' }, },
-            workspace = {
+            --[[ workspace = {
                 library = {
                     vim.api.nvim_get_runtime_file("", true),
                     [vim.fn.expand("$VIMRUNTIME/lua/vim")] = true,
                     [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-                },
-            }
-		},
-	}
+                }, ]]
+
+            -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+            -- diagnostics = { disable = { 'missing-fields' } },
+        },
+    },
+    -- gopls = {},
+    -- rust_analyzer = {},
 }
 
+-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+-- Ensure the servers above are installed
+local mason_lspconfig = require 'mason-lspconfig'
+
+mason_lspconfig.setup {
+  ensure_installed = vim.tbl_keys(servers),
+}
+
+mason_lspconfig.setup_handlers {
+  function(server_name)
+    require('lspconfig')[server_name].setup {
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = servers[server_name],
+      filetypes = (servers[server_name] or {}).filetypes,
+    }
+  end,
+}
