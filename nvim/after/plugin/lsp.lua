3b01838f5f3309e18b358e52521307f26ee502e9
Petr Pacner <petr.pacner@cern.ch>
2024-03-04 10:31:09 +0100

    added lsp controls

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,16 +1,12 @@
 require("mason").setup()
 local mason_lspconfig = require('mason-lspconfig')
 
-local servers = {verible = {}}
+local servers = {verible = {}, clangd = {}}
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
-}
-
-local on_attach = function(bufnr)
+local on_attach = function(client, bufnr)
     local gs = package.loaded.gitsigns
 
     local function map(mode, l, r, opts)
@@ -63,6 +59,22 @@ local on_attach = function(bufnr)
         gs.diffthis '~'
     end, { desc = 'git diff against last commit' })
 
+    -- lsp control
+    map('n', 'gd', require('telescope.builtin').lsp_definitions, { desc = '[G]oto [D]efinition' })
+    map('n', 'gr', require('telescope.builtin').lsp_references, { desc = '[G]oto [R]eferences' })
+    map('n', 'gI', require('telescope.builtin').lsp_implementations, { desc = '[G]oto [I]mplementation' })
+    map('n', '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, { desc = '[W]orkspace [S]ymbols' })
+    map('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols, { desc = '[D]ocument [S]ymbols' })
+    map('n', '<leader>D', require('telescope.builtin').lsp_type_definitions, { desc = 'Type [D]efinition' })
+
+    map('n', '<leader>rn', vim.lsp.buf.rename, { desc = '[R]e[n]ame' })
+
+    map('n', '<leader>ca', vim.lsp.buf.code_action, { desc = '[C]ode [A]ction' })
+
+    map('n', 'K', vim.lsp.buf.hover, { desc = 'Hover Documentation' })
+
+    map('n', 'gD', vim.lsp.buf.declaration, { desc = '[G]oto [D]eclaration' })
+
     -- Toggles
     map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
     map('n', '<leader>td', gs.toggle_deleted, { desc = 'toggle git show deleted' })
@@ -80,6 +92,11 @@ mason_lspconfig.setup_handlers {
       filetypes = (servers[server_name] or {}).filetypes,
     }
   end,
+
+}
+
+mason_lspconfig.setup {
+  ensure_installed = vim.tbl_keys(servers),
 }
 
 require("neodev").setup()
