e6a42cd8a65e8eb03ef4c126890ca7f7df7bb479
dodotronix <dodotronix@protonmail.com>
2026-05-20 12:17:30 +0200

    updated treesitter

--- nvim/after/plugin/globals.lua
+++ nvim/after/plugin/globals.lua
@@ -11,3 +11,14 @@ R = function (name)
    RELOAD(name) 
    return require(name)
 end
+
+vim.diagnostic.config({
+    -- Set to true to see inline messages
+    virtual_text = true,
+
+    -- Set to true to see icons in the gutter
+    signs = true,
+
+    -- Set to true to underline the problematic text
+    underline = true,
+})
--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,7 +1,13 @@
-require('nvim-treesitter.configs').setup {
-	ensure_installed = { "norg", "query", "verilog", "c", "lua", "python" },
-	highlight = { enable = true, },
-    autoinstall = true,
+require('nvim-treesitter').setup {
+    ensure_installed = { "norg", "query", "verilog", "c", "lua", "python", "markdown", "markdown_inline", "vim", "vimdoc" },
+    
+    prefer_git = true, 
+    
+    highlight = { 
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+    auto_install = true, 
     indent = { enable = true },
     incremental_selection = {
         enable = true,
@@ -12,22 +18,4 @@ require('nvim-treesitter.configs').setup {
             node_decremental = '<M-space>',
         },
     },
-    playground = {
-        enable = true,
-        disable = {},
-        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-        persist_queries = false, -- Whether the query persists across vim sessions
-        keybindings = {
-            toggle_query_editor = 'o',
-            toggle_hl_groups = 'i',
-            toggle_injected_languages = 't',
-            toggle_anonymous_nodes = 'a',
-            toggle_language_display = 'I',
-            focus_language = 'f',
-            unfocus_language = 'F',
-            update = 'R',
-            goto_node = '<cr>',
-            show_help = '?',
-        },
-    }
 }
